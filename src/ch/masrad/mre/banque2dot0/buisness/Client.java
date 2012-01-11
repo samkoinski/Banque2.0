83f0b36da26961c7b2678bbe458d153b8ea23485
sam koinski <sam.koinski@laposte.net>
2012-01-11 21:26:26 +0100

    added DAO exemple for person work not finished

--- src/ch/masrad/mre/banque2dot0/buisness/Client.java
+++ src/ch/masrad/mre/banque2dot0/buisness/Client.java
@@ -2,21 +2,30 @@ package ch.masrad.mre.banque2dot0.buisness;
 
 public class Client {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Person paul = new Person("Paul");
-		Person georges = new Person("Georges");
-		Bank aBank = new Bank();
-		
-		Account paulsAccount = new Account(paul);
-		Account georgsAccount = new Account(georges); 
-		
-		System.out.println(paulsAccount.getBalance());
-		aBank.Transaction(paulsAccount,georgsAccount,10);
-		System.out.println(paulsAccount.getBalance());
-		System.out.println(georgsAccount.getBalance());
-	}
+ public static void main(String[] args) {
+    //Person paul = new Person(2,"Pierre");
 
-}
+    //DAO<Person> paulDAO = new PersonDAO();
+
+    //paulDAO.create(paul);
+
+    //Person georges = new Person("Georges");
+    //Bank aBank = new Bank();
+
+    //Account paulsAccount = new Account(paul);
+    //Account georgsAccount = new Account(georges); 
+
+    //System.out.println(paulsAccount.getBalance());
+    //aBank.Transaction(paulsAccount,georgsAccount,10);
+    //System.out.println(paulsAccount.getBalance());
+    //System.out.println(georgsAccount.getBalance());
+              
+    DAO<Person> personDAO = new PersonDAO();
+    Person paul = new Person();
+    paul.setName("Jacques");
+    paul = personDAO.create(paul);
+    
+    System.out.println(paul.getId());
+    
+    }
+ }
