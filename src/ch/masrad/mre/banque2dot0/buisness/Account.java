b70b7d5b91fadf38367f41bbfa6b1bf7ecfee1e3
sam koinski <sam.koinski@laposte.net>
2012-01-21 18:25:29 +0100

    test

--- src/ch/masrad/mre/banque2dot0/buisness/Account.java
+++ src/ch/masrad/mre/banque2dot0/buisness/Account.java
@@ -1,50 +1,56 @@
 package ch.masrad.mre.banque2dot0.buisness;
 
-public class Account {
-    
-        private int _id;
-	private Person _owner;
-	private int _balance = 0;
-        
-        public Account (int id, int balance, int owner_id) {
-            _id = id;
-            _balance = balance;
-            
-            DAO<Person> personDAO = new PersonDAO();
-            _owner = new Person();
-            _owner = personDAO.find(owner_id);
-        }
-        
-	public Account (Person p) {
-		_owner = p;
-	}
-        
-        public Account () {
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class Account {
+
+    private int _id;
+    private Person _owner;
+    private int _balance = 0;
+
+    public Account(int id, int balance, int owner_id) {
+        _id = id;
+        _balance = balance;
+
+        DAO<Person> personDAO = new PersonDAO();
+        _owner = new Person();
+        try {
+            _owner = personDAO.find(owner_id);
+        } catch (SQLException ex) {
+            Logger.getLogger(Account.class.getName()).log(Level.SEVERE, null, ex);
         }
-	
-	public void credit(int i){
-		_balance += i;
-	}
-        
-	public void debit(int i){
-		_balance -= i;
-	}
-        
-        public int getId (){
-		return _id;
-	}
-        
-        public Person getOwner (){
-		return _owner;
-	}
-        
-	public int getBalance(){
-                return _balance;
-	}
-        
-        public void setOwner (Person new_owner){
-		_owner = new_owner;
-	}
-        
+    }
+
+    public Account(Person p) {
+        _owner = p;
+    }
+
+    public Account() {
+    }
+
+    public void credit(int i) {
+        _balance += i;
+    }
+
+    public void debit(int i) {
+        _balance -= i;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    public Person getOwner() {
+        return _owner;
+    }
+
+    public int getBalance() {
+        return _balance;
+    }
+
+    public void setOwner(Person new_owner) {
+        _owner = new_owner;
+    }
 }
