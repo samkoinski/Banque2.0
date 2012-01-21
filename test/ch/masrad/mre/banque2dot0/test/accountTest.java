2fc1dc6a65f4dd7890ab0ff689f7db0afc1a2fd4
sam koinski <sam.koinski@laposte.net>
2012-01-21 18:27:47 +0100

    fixed Person Test and added exception handling

--- test/ch/masrad/mre/banque2dot0/test/accountTest.java
+++ test/ch/masrad/mre/banque2dot0/test/accountTest.java
@@ -1,9 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package ch.masrad.mre.banque2dot0.test;
 
 import ch.masrad.mre.banque2dot0.buisness.*;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,10 +16,10 @@ import static org.junit.Assert.*;
  * @author phanuel
  */
 public class accountTest {
-    
+
     private Person _pers;
     private Account _acnt;
-    
+
     public accountTest() {
     }
 
@@ -30,75 +30,91 @@ public class accountTest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
-        // creating a new person named "Paul"
-        _pers = new Person();
-        _pers.setName("Paul");
-        
-        DAO<Person> personDAO = new PersonDAO();
-        _pers = personDAO.create(_pers);
-        
-        _acnt = new Account();
+        try {
+            // creating a new person named "Paul"
+            _pers = new Person();
+            _pers.setName("Paul");
+
+            DAO<Person> personDAO = new PersonDAO();
+            _pers = personDAO.create(_pers);
+
+            _acnt = new Account();
+        } catch (SQLException ex) {
+            Logger.getLogger(accountTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
     @Test
     public void testAssign() {
         // assigning a new account to Paul
         _acnt.setOwner(_pers);
-        
+
         assertEquals(_acnt.getOwner(), _pers);
     }
-    
+
     @Test
     public void testInitialBalance() {
         // getting initial balance ammount
         assertTrue(_acnt.getBalance() == 0);
     }
-    
+
     @Test
     public void testCredit() {
         // crediting an ammount to Paul's account
         _acnt.credit(100);
         assertTrue(_acnt.getBalance() == 100);
     }
-    
+
     @Test
     public void testDebit() {
         // debiting an ammount from Paul's account
         _acnt.debit(100);
         assertTrue(_acnt.getBalance() == -100);
     }
-    
+
     @Test
     public void testCreate() {
-        DAO<Account> acntDAO = new AccountDAO();
-        _acnt = acntDAO.create(_acnt);
-        assertEquals(_acnt.getId(), acntDAO.find(_acnt.getId()).getId());
+        try {
+            DAO<Account> acntDAO = new AccountDAO();
+            _acnt = acntDAO.create(_acnt);
+            assertEquals(_acnt.getId(), acntDAO.find(_acnt.getId()).getId());
+        } catch (SQLException ex) {
+            Logger.getLogger(accountTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     @Test
     public void testUpdate() {
-        DAO<Account> acntDAO = new AccountDAO();
-        _acnt = acntDAO.create(_acnt);
-        _acnt.credit(100);
-        _acnt = acntDAO.update(_acnt);
-        assertTrue(_acnt.getBalance() == 100);
+        try {
+            DAO<Account> acntDAO = new AccountDAO();
+            _acnt = acntDAO.create(_acnt);
+            _acnt.credit(100);
+            _acnt = acntDAO.update(_acnt);
+            assertTrue(_acnt.getBalance() == 100);
+        } catch (SQLException ex) {
+            Logger.getLogger(accountTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     @Test
     public void testDelete() {
-        DAO<Account> acntDAO = new AccountDAO();
-        _acnt = acntDAO.create(_acnt);
-        assertEquals(_acnt.getId(), acntDAO.find(_acnt.getId()).getId());
-        
-        acntDAO.delete(_acnt);
-        Account acnt = acntDAO.find(_acnt.getId());
-        assertNotSame(acnt.getId(), _acnt.getId());
+        try {
+            DAO<Account> acntDAO = new AccountDAO();
+            _acnt = acntDAO.create(_acnt);
+            assertEquals(_acnt.getId(), acntDAO.find(_acnt.getId()).getId());
+
+            acntDAO.delete(_acnt);
+            Account acnt = acntDAO.find(_acnt.getId());
+            assertNotSame(acnt.getId(), _acnt.getId());
+        } catch (SQLException ex) {
+            Logger.getLogger(accountTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- test/ch/masrad/mre/banque2dot0/test/personTest.java
+++ test/ch/masrad/mre/banque2dot0/test/personTest.java
@@ -1,4 +1,9 @@
+package ch.masrad.mre.banque2dot0.test;
+
 import ch.masrad.mre.banque2dot0.buisness.*;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,58 +16,88 @@ import static org.junit.Assert.*;
  * @author samkoinski
  */
 public class personTest {
-    
+
     DAO<Person> personDAO = new PersonDAO();
     Person customer = new Person();
-        
-    
+    Person customerOut = new Person();
+
     public personTest() {
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-
-
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
         customer.setName("paul");
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    
     @Test
     public void testName() {
-            assertEquals("paul", customer.getName());
+        assertEquals("paul", customer.getName());
     }
-    
+
     @Test
     public void createPerson() {
+        try {
+            customer.setName("Test Person " + System.currentTimeMillis());
+            customer = personDAO.create(customer);
+            assertEquals(customer.getId(), customer.getId());
+            if (customer.getId() == 0) {
+                fail("Customer not created");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(personTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-        customer = personDAO.create(customer);
-        assertEquals("paul", customer.getName());
     }
-    
+
     @Test
     public void findPerson() {
-        
-        customer = personDAO.find(customer.getId());
-        assertEquals("paul", customer.getName());
+        try {
+            customer = personDAO.find(1);
+            assertEquals(1, customer.getId());
+        } catch (SQLException ex) {
+            Logger.getLogger(personTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
     @Test
     public void updatePerson() {
-        
-        customer.setName("Yves");
-        customer = personDAO.update(customer);
-        assertEquals("Yves", customer.getName());
+        try {
+            customer = personDAO.find(1);
+            customer.setName("Test Update" + System.currentTimeMillis());
+            customerOut = personDAO.update(customer);
+            assertEquals(customer.getName(), customerOut.getName());
+            assertEquals(customer.getId(), customerOut.getId());
+        } catch (SQLException ex) {
+            Logger.getLogger(personTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @Test
+    public void deletePerson() {
+        try {
+            customer.setName("Test Person" + System.currentTimeMillis());
+            customer = personDAO.create(customer);
+            personDAO.delete(customer);
+            customer = personDAO.find(customer.getId());
+            if (customer.getId() != 0) {
+                fail("Customer not deleted");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(personTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
